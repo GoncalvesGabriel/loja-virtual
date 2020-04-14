77c91860ba63a0ce2b7c2612c2cc90f78dce9241
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-04-14 19:48:32 -0300

    LJV-1 Commit com config iniciais da aplicaçao

---
+++ src/main/java/br/com/lojavirtual/config/ConfigJPA.java
@@ -0,0 +1,12 @@
+package br.com.lojavirtual.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("br.com.lojavirtual")
+public class ConfigJPA {
+
+}
