e68901158963697e7b4c26a9d382d13055b1365e
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-04-17 00:38:54 -0300

    LJV-8 Alteção em Renner de testes de controller

--- src/test/java/br/com/lojavirtual/controller/OrderControllerTest.java
+++ src/test/java/br/com/lojavirtual/controller/OrderControllerTest.java
@@ -17,12 +17,13 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class OrderControllerTest {
 
   private MockMvc mockMvc;
--- src/test/java/br/com/lojavirtual/controller/ProductControllerTest.java
+++ src/test/java/br/com/lojavirtual/controller/ProductControllerTest.java
@@ -3,7 +3,6 @@ package br.com.lojavirtual.controller;
 import static br.com.lojavirtual.TestHelper.asJsonString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import br.com.lojavirtual.config.ConfigMain;
 import br.com.lojavirtual.dto.product.CreateProductDTO;
 import br.com.lojavirtual.dto.product.ProductDTO;
 import br.com.lojavirtual.service.product.ProductService;
@@ -13,19 +12,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class ProductControllerTest {
 
   private MockMvc mockMvc;
