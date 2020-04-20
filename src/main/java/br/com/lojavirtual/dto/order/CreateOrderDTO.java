9779e57f0b7a9589af1f87ae07a39f89d7dc0b62
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-04-19 21:29:48 -0300

    LJV-9 mais documentação

--- src/main/java/br/com/lojavirtual/dto/order/CreateOrderDTO.java
+++ src/main/java/br/com/lojavirtual/dto/order/CreateOrderDTO.java
@@ -1,6 +1,7 @@
 package br.com.lojavirtual.dto.order;
 
 
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 public @Data
 class CreateOrderDTO {
 
+  @ApiModelProperty(value = "Lista de itens")
   private List<CreateOrderItemDTO> itens;
 
 }
